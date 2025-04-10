0399ac32fe002f7f8af7204e915a9f42b2bcafb7
Rithip-Rajavel <rithipcegian@gmail.com>
2025-04-10 15:25:54 +0530

    initial commit : my_learning

---
+++ App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfLearning;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private void Application_Startup(object sender, StartupEventArgs e)
+    {
+        MainWindow main = new MainWindow();
+        if (e.Args.Length == 1)
+        {
+            MessageBox.Show("Now Opening File: \n\n" + e.Args[0]);
+        }
+        main.Show();
+    }
+}
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLearning;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    // private void Button_Click(object sender, RoutedEventArgs e)
+    // {
+    //     string text = InputTextBox.Text;
+    //     MessageBox.Show($"You entered: {text}");
+    // }
+    private void MainGrid_MouseUp(Object sender,MouseButtonEventArgs e)
+    {
+        
+        // if (e.ChangedButton == MouseButton.Left)
+        // {
+        //     MessageBox.Show("Left mouse button clicked!");
+        // }
+        // else if (e.ChangedButton == MouseButton.Right)
+        // {
+        //     MessageBox.Show("Right mouse button clicked!");
+        // }
+
+        MessageBox.Show("You Clicked me at"+e.GetPosition(this).ToString());
+    }
+
+}
